8cec54f3611eabe7e0b14ce5097ba20309168c8f
unknown <NNJ@GAZ.bsrv.com.ua>
2021-08-08 15:28:17 +0300

    commit one

---
+++ js/my-scripts.js
@@ -0,0 +1,14 @@
+$('.hero-slider').slick({
+	 arrows: true,
+	 dots: true,
+});
+
+$('.star-slider__grid').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots: true,
+});
+
+
+
